8f90d62a1cfbd13a6d6bd203e2a64c0c079a4ab2
freakyluke <freaky_luke@hotmail.co.uk>
2025-08-24 17:37:38 +0100

    updated refresh rate

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt=0
     while 1==1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) /1000
+
 
 
 if __name__ == "__main__":
